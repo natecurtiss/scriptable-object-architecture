096a0550ff370a4ebd998efeb34be964cdd6a5c5
Walter Hulsebos <walterhulsebos@outlook.com>
2021-11-08 13:35:39 +0100

    NEW: (Ref) Implicit Conversion from/to T & TVar

--- Runtime/Refs/Ref.cs
+++ Runtime/Refs/Ref.cs
@@ -63,13 +63,31 @@ namespace N8.Utils.SOA
             this.useDefault = useDefault;
             this.variable = variable;
         }
+        */
+
+        #endregion
+
+        #region Methods
+
+        public override String ToString() => Value.ToString();
 
         #endregion
 
         #region Operators
 
+        //TODO: Conversion Operators default check.
+        
         [MethodImpl(AggressiveInlining)]
-        public static implicit operator T(Ref<T, TVar> input) => input.Value;
+        public static implicit operator T (Ref<T, TVar> input) => input.Value;
+        
+        [MethodImpl(AggressiveInlining)]
+        public static implicit operator TVar (Ref<T, TVar> input) => input.variable;
+        
+        [MethodImpl(AggressiveInlining)]
+        public static implicit operator Ref<T, TVar> (T value) => new Ref<T, TVar>(value);
+        
+        [MethodImpl(AggressiveInlining)]
+        public static implicit operator Ref<T, TVar> (TVar variable) => new Ref<T, TVar>(variable);
 
         #endregion
     }
