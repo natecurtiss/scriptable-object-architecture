049fe5fef2995caafabb682ae64e2beeaa2bf553
Walter Hulsebos <walterhulsebos@outlook.com>
2021-11-13 14:58:56 +0100

    NEW: (VarTests) New_Empty & New_T

--- Tests/Editor/VarTests.cs
+++ Tests/Editor/VarTests.cs
@@ -7,6 +7,62 @@ namespace N8.Utils.SOA.Tests.Editor
 	
 	public class VarTests
 	{
+		/*
+		[Test]
+		public void New_Empty()
+		{
+			Var<Vector3> __vector3Var = Var<Vector3>.New();
+		    
+			Assert.AreEqual(
+				expected: Vector3.zero, 
+				actual: __vector3Var.Value);
+		    
+			Assert.AreNotEqual(
+				expected: Vector3.one, 
+				actual: __vector3Var.Value);
+		}
+
+		[Test]
+		public void New_T()
+		{
+			Var<Vector3> __vector3Var = Var<Vector3>.New(val: Vector3.one);
+		    
+			Assert.AreEqual(
+				expected: Vector3.one, 
+				actual: __vector3Var.Value);
+		    
+			Assert.AreNotEqual(
+				expected: Vector3.zero, 
+				actual: __vector3Var.Value);
+		}
+		*/
 		
+		[Test]
+		public void New_Empty()
+		{
+			Vector3Var __vector3Var = Vector3Var.New();
+            
+			Assert.AreEqual(
+				expected: Vector3.zero, 
+				actual: __vector3Var.Value);
+            
+			Assert.AreNotEqual(
+				expected: Vector3.one, 
+				actual: __vector3Var.Value);
+		}
+        
+		[Test]
+		public void New_T()
+		{
+			Vector3Var __vector3Var = Vector3Var.New(val: Vector3.one);
+            
+			Assert.AreEqual(
+				expected: Vector3.one, 
+				actual: __vector3Var.Value);
+            
+			Assert.AreNotEqual(
+				expected: Vector3.zero, 
+				actual: __vector3Var.Value);
+		}
 	}
 }
