59af632a8ee9641bd44f53c885a466e0098d2a43
Walter Hulsebos <walterhulsebos@outlook.com>
2021-11-13 13:26:13 +0100

    NEW: class - Vector3Var

---
+++ Runtime/Vars/Vector3Var.cs
@@ -0,0 +1,11 @@
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace N8.Utils.SOA.Vars
+{
+	[CreateAssetMenu(fileName = "NewVector3Var", menuName = "N8Dev/SOA/Variables/Vector3"), InlineEditor]
+	public sealed class Vector3Var : Var<Vector3>
+	{
+
+	}
+}
