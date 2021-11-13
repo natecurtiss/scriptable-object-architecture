c03f13b4802385099e9d0778f7f3f696a2abfdde
Walter Hulsebos <walterhulsebos@outlook.com>
2021-11-13 14:54:45 +0100

    NEW: (Vector3Var) Methods New & New(val) as faux constructors.

--- Runtime/Vars/Vector3Var.cs
+++ Runtime/Vars/Vector3Var.cs
@@ -6,6 +6,24 @@ namespace N8.Utils.SOA.Vars
 	[CreateAssetMenu(fileName = "NewVector3Var", menuName = "N8Dev/SOA/Variables/Vector3"), InlineEditor]
 	public sealed class Vector3Var : Var<Vector3>
 	{
+		#region "Structors"
 
+		public new static Vector3Var New()
+		{
+			Vector3Var newVar = CreateInstance<Vector3Var>();
+
+			return newVar;
+		}
+        
+		public new static Vector3Var New(Vector3 val)
+		{
+			Vector3Var newVar = CreateInstance<Vector3Var>();
+
+			newVar.Value = val;
+
+			return newVar;
+		}
+
+		#endregion
 	}
 }
