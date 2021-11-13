a86ae2efc7b64bc590016340217d88af3e2f9b70
Walter Hulsebos <walterhulsebos@outlook.com>
2021-11-13 14:52:37 +0100

    CHANGE: (Var) Added regions to code
    + Attempt at "Constructors"

--- Runtime/Vars/Var.cs
+++ Runtime/Vars/Var.cs
@@ -5,17 +5,53 @@ namespace N8.Utils.SOA.Vars
 {
     public abstract class Var<T> : ScriptableObject, ISerializationCallbackReceiver
     {
+        #region Fields & Properties
+
         [SerializeField]
         private T _value;
         
-        [field: NonSerialized] //Shouldn't be serialized anyhow, even with Odin installed, right?
         public T Value { get; set; }
+
+        #endregion
+
+        /*
+        //TODO: Figure these out.
+        #region "Structors"
+
+        public static Var<T> New()
+        {
+            Var<T> newVar = CreateInstance<Var<T>>();
+
+            return newVar;
+        }
+        
+        public static Var<T> New(T val)
+        {
+            Var<T> newVar = CreateInstance<Var<T>>();
+
+            newVar.Value = val;
+
+            return newVar;
+        }
+        
+        #endregion
+        */
+        
+        #region Methods
 
         public void OnBeforeSerialize() => Value = _value;
         public void OnAfterDeserialize() { }
         
-        public static implicit operator T(Var<T> input) => input.Value;
         
-        public override string ToString() => Value.ToString();
+        public override String ToString() => Value.ToString();
+
+        #endregion
+
+        #region Operators
+        
+        //Can't work in reverse because ScriptableObjects can't be created via constructor.
+        public static implicit operator T(Var<T> input) => input.Value;
+
+        #endregion
     }
 }
